8347c46128d98c2ffabe822c08feb38df24f2acb
syed-m-asim <asimidforproject@gmail.com>
2025-12-05 17:33:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let inp = document.getElementById("input");
+let task = document.getElementById("add");
+task.addEventListener("click", function (a) {
+    a.preventDefault();
+    console.log(inp.value)
+});
+
+
+
